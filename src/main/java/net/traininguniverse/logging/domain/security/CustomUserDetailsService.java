01b4ee70cc686b2337b9bd621eb0406a81449b7d
traininguniverse <treninguniverse@gmail.com>
2022-07-06 13:44:45 +0200

    implement authentication - UserDetails(Service)

---
+++ src/main/java/net/traininguniverse/logging/domain/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package net.traininguniverse.logging.domain.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import net.traininguniverse.logging.domain.User;
+import net.traininguniverse.logging.repository.UserRepository;
+ 
+public class CustomUserDetailsService implements UserDetailsService {
+ 
+    @Autowired
+    private UserRepository userRepo;
+     
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new CustomUserDetails(user);
+    }
+ 
+}
--- src/main/java/net/traininguniverse/logging/repository/UserRepository.java
+++ src/main/java/net/traininguniverse/logging/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package net.traininguniverse.logging.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import net.traininguniverse.logging.domain.User;
 
 public interface UserRepository extends JpaRepository<User,Long>{
+
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	public User findByEmail(String username);
 
 }
