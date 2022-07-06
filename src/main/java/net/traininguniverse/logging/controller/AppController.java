c6316e14683225f00e24631f215203accc8a8fe6
traininguniverse <treninguniverse@gmail.com>
2022-07-06 13:30:20 +0200

    implement user registration feauture

--- src/main/java/net/traininguniverse/logging/controller/AppController.java
+++ src/main/java/net/traininguniverse/logging/controller/AppController.java
@@ -1,9 +1,13 @@
 package net.traininguniverse.logging.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import net.traininguniverse.logging.domain.User;
 import net.traininguniverse.logging.repository.UserRepository;
 
 @Controller
@@ -11,12 +15,26 @@ public class AppController {
 
 	@Autowired
 	UserRepository userRepo;
-	
-	
+
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
 	}
-	
-	
+
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
+	}
+
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+
+		userRepo.save(user);
+
+		return "register_success";
+	}
 }
